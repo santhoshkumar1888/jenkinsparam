67d5ed2faad782ab736cb843f536b162ce897c2d
santhoshkumar1888 <santuu888@gmail.com>
2025-09-28 18:12:43 +0530

    wel

--- src/test/java/day1/Demo1.java
+++ src/test/java/day1/Demo1.java
@@ -12,19 +12,27 @@ import org.testng.Reporter;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.reporter.ExtentSparkReporter;
+
 public class Demo1 {
 	@Parameters({"gridURL","appURL"})
 	@Test
 	public void testa(String gridURL, String appURL) throws InterruptedException, MalformedURLException {
-		Reporter.log("Testamethod",true);
+		ExtentReports report=new ExtentReports();
+		ExtentSparkReporter spark=new ExtentSparkReporter("report/SparkTest.html");
+		report.attachReporter(spark);
+		ExtentTest extest=report.createTest("To Login facebook");
+		extest.info("Facebook logining");
 		WebDriver driver=new RemoteWebDriver(new URL(gridURL),new ChromeOptions());
 		driver.get(appURL);
-		System.out.println(driver.getTitle());
+		extest.info(driver.getTitle());
 		driver.findElement(By.id("email")).sendKeys("9740971888");
 		driver.findElement(By.id("pass")).sendKeys("santhosh8");
 		driver.findElement(By.name("login")).click();
 		Thread.sleep(7000);
-		System.out.println(driver.getTitle());
+		extest.info(driver.getTitle());
 		driver.close();
 		
 	}
