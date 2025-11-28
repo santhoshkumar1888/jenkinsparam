3fca679571f99d236ebc556fd0e5289b4ea79a02
santhoshkumar1888 <santuu888@gmail.com>
2025-11-28 12:43:19 +0530

    upd

--- src/test/java/day1/Demo1.java
+++ src/test/java/day1/Demo1.java
@@ -1,6 +1,7 @@
 package day1;
 
 import java.net.MalformedURLException;
+import java.net.URI;
 import java.net.URL;
 
 import org.openqa.selenium.By;
@@ -19,12 +20,17 @@ public class Demo1 {
 	@Parameters({"gridURL","appURL"})
 	@Test
 	public void testa(String gridURL, String appURL) throws InterruptedException, MalformedURLException {
+		URI uri = URI.create(gridURL);
+		URL url = uri.toURL();
+
+		
 		ExtentReports report=new ExtentReports();
 		ExtentSparkReporter spark=new ExtentSparkReporter("./report/Spark.html");
 		report.attachReporter(spark);
 		ExtentTest extest=report.createTest("TestA");
 		extest.info("Facebook logining");
-		WebDriver driver=new RemoteWebDriver(new URL(gridURL),new ChromeOptions());
+//		WebDriver driver=new RemoteWebDriver(new URL(gridURL),new ChromeOptions());
+		WebDriver driver = new RemoteWebDriver(url, new ChromeOptions());
 		driver.get(appURL);
 		extest.info(driver.getTitle());
 		driver.findElement(By.id("email")).sendKeys("9740971888");
